52327387a2058f476fa0b26b35bd3d4882573a19
kimjunsseop <133566940+kimjunsseop@users.noreply.github.com>
2023-07-20 14:47:51 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>	
+#include <time.h>
+
+
+
+int main()
+{
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )");
+	printf("\"");
+	printf("\"");
+	printf("\"");
+	printf("\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|");
+
+
+
+}
+
+
+
