83c57f45a1a41a8cc7d7df450ff2e9a9305b8d77
kimjunsseop <133566940+kimjunsseop@users.noreply.github.com>
2023-07-20 15:55:38 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>	
+#include <time.h>
+
+
+int main()
+{
+	int a, b;
+	while (scanf("%d%d",&a,&b) !=EOF)
+	{
+		
+		printf("%d\n", a + b);
+	
+	}
+	
+}
+
+
