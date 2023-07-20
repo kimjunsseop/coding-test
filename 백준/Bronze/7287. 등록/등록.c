b9432b9b76489e9b88959e92440b37bb2dac0aaa
kimjunsseop <133566940+kimjunsseop@users.noreply.github.com>
2023-07-20 14:33:36 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>	
+#include <time.h>
+
+
+
+int main()
+{
+	printf("%d\n", 44);
+	printf("kimjs33350");
+
+
+}
