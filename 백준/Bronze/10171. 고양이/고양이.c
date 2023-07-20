144a4d0531ef7a3f7211658a6e5f11628b2b83f8
kimjunsseop <133566940+kimjunsseop@users.noreply.github.com>
2023-07-20 14:40:20 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>	
+#include <time.h>
+
+
+
+int main()
+{
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|");
+
+
+}
+
+
+
+
